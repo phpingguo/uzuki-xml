ea56226150bb718fd0c7e5511c16f31746d257cd
h-sugawara <nnfxt540@gmail.com>
2014-06-04 20:48:20 +0900

    added UzukiXml class unit test file

---
+++ tests/src/UzukiXmlTest.php
@@ -0,0 +1,61 @@
+<?php
+namespace Phpingguo\UzukiXml\Tests;
+
+use Phpingguo\UzukiXml\UzukiXml;
+
+class UzukiXmlTest extends \PHPUnit_Framework_TestCase
+{
+    public function providerRender()
+    {
+        return [
+            [ [] ],
+            [ [ 'name' => 'hogehoge' ] ],
+            [ [ 'members' => [ [ 'name' => 'hogehoge' ], [ 'name' => 'foobar' ] ] ] ]
+        ];
+    }
+
+    /**
+     * @dataProvider providerRender
+     */
+    public function testRender($variables)
+    {
+        $options = [
+            'SuperParentName'     => 'xml_body',
+            'DefaultListItemName' => 'list_item',
+        ];
+        $uzuki = new UzukiXml($options);
+
+        $expected = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+
+        if (count($variables) > 0) {
+            $expected .= '<xml_body>';
+
+            foreach ($variables as $key => $item) {
+                if (is_array($item)) {
+                    $tag_name = $options['DefaultListItemName'];
+                    $expected .= "<{$key}>";
+
+                    foreach ($item as $sub_item) {
+                        $expected .= "<{$tag_name}>";
+
+                        foreach ($sub_item as $sub_key => $sub_value) {
+                            $expected .= "<{$sub_key}>{$sub_value}</{$sub_key}>";
+                        }
+
+                        $expected .= "</{$tag_name}>";
+                    }
+
+                    $expected .= "</{$key}>";
+                } else {
+                    $expected .= "<{$key}>{$item}</{$key}>";
+                }
+            }
+
+            $expected .= '</xml_body>' . "\n";
+        } else {
+            $expected .= '<xml_body/>' . "\n";
+        }
+
+        $this->assertSame($expected, $uzuki->render($variables));
+    }
+}
